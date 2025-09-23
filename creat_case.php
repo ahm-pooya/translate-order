e1468c4cbf3fa9f644b2315b8e36615d88a6553a
pooya <ahm.pooya@gmail.com>
2025-09-23 16:52:34 +0800

    final

--- creat_case.php
+++ creat_case.php
@@ -1,14 +1,16 @@
 <?php
-$task_uid = '17644980468b06b8708ea32002405643';
-$group_id = '21435359168d0655a3b1ee9098721494';
-$process_id = @@PROCESS;
-//$parent_case_id = @@APPLICATION;
-$translators = executeQuery("SELECT USR_UID FROM GROUP_USER WHERE GRP_UID = '$group_id'");
-$vars = array(
-    'txt_order' => @@txt_order,
-    //'parent_case_id' => $parent_case_id,
-);
-foreach ($translators as $user) {
-    $translator_id = $user['USR_UID'];
-    PMFNewCase($process_id, $translator_id, $task_uid, $vars, "TO_DO");
+if (@@rad_confirm_order == 1) {
+    $task_uid = '17644980468b06b8708ea32002405643';
+    $group_id = '21435359168d0655a3b1ee9098721494';
+    $process_id = @@PROCESS;
+    //$parent_case_id = @@APPLICATION;
+    $translators = executeQuery("SELECT USR_UID FROM GROUP_USER WHERE GRP_UID = '$group_id'");
+    $vars = array(
+        'txt_order' => @@txt_order,
+        //'parent_case_id' => $parent_case_id,
+    );
+    foreach ($translators as $user) {
+        $translator_id = $user['USR_UID'];
+        PMFNewCase($process_id, $translator_id, $task_uid, $vars, "TO_DO");
+    }
 }
---
+++ get_translator_id.php
@@ -0,0 +1,1 @@
+@@translator_id = @@USER_LOGGED;
